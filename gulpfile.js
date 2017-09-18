afd0e740a48e36440305a7b79d032f99d1ff0767
tovi-cn <1018496570@qq.com>
2017-09-18 23:56:06 +0800

    #

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 //载入核心包
 const gulp = require('gulp')
-
+var sass = require('gulp-sass')
 //定义一个任务
 //第一个参数任务名，第二个参数是任务执行体
 gulp.task('dest',function(){
@@ -12,7 +12,12 @@ gulp.task('dest',function(){
 //default为gulp默认执行的任务
 
 gulp.task('default',function(){
-    //获取文件
-    gulp.src('./src/**/*.*')
-      .pipe(gulp.dest('dist/'))
+    gulp.watch('./src/sass/*.*', ['sass']);
 })
+
+gulp.task('sass', function() {
+    //获取文件
+    gulp.src('./src/sass/*.*')
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(gulp.dest('./dist/css/'))
+});
